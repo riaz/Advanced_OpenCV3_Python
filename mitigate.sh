346a72471a3951a43bc8cfe51e69ad145e3b2544
rmunshi <rmunshi@yahoo-inc.com>
2017-10-20 03:08:46 -0700

    Section3 notebooks

--- mitigate.sh
+++ mitigate.sh
@@ -1,1 +1,8 @@
+#Installing the vim editor
 apt-get install vim
+
+#Installing the selenium headless browser
+#pip install selenium
+
+pip install ipyleaflet
+jupyter nbextension enable --py --sys-prefix ipyleaflet
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #docker build -no-cache -t aop .
 
-docker run -it --rm -p 8888:8888  -v `pwd`:/src aop /bin/bash 
+docker run -it --rm -p 8888:8888  -v `pwd`:/src aop /bin/bash
+
+#docker run -it --rm -p 8888:8888 -v /dev/video0:/dev/video0  -v /tmp/.X11-unix:/tmp/.X11-unix:ro  -v `pwd`:/src aop /bin/bash 
