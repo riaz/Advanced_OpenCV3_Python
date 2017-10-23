3fb6e7bbc0d11ad79057e727c17dca241f8eb1f5
rmunshi <rmunshi@yahoo-inc.com>
2017-10-22 19:04:24 -0700

    Code Video Ready

--- mitigate.sh
+++ mitigate.sh
@@ -1,8 +1,12 @@
 #Installing the vim editor
-apt-get install vim
+apt-get -y install vim
 
 #Installing the selenium headless browser
 #pip install selenium
 
 pip install ipyleaflet
 jupyter nbextension enable --py --sys-prefix ipyleaflet
+
+# needed to be able to play videos in the notebook
+pip install imageio
+pip install moviepy
